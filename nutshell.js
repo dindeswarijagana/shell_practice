76cf200e191382672ee523513201302f479281e2
Dindeswari Jagana <183721475+dindeswarijagana@users.noreply.github.com>
2024-12-19 18:25:06 +0530

    adding ls and touch commands

--- nutshell.js
+++ nutshell.js
@@ -1,5 +1,16 @@
 let currDirectory = "~";
 const promptText = "nutshell ";
+const files = [];
+
+const runLs = function () {
+  return files.join("\n");
+}
+
+const runTouch = function (args) {
+  files.push(args);
+  console.log(files);
+  console.log("file created");
+}
 
 const runEcho = function (args) {
   return args.join(" ");
@@ -15,6 +26,8 @@ const runCommand = function (runningCommand) {
   switch (command) {
     case "echo": return runEcho(args);
     case "cd": return runCd(args);
+    case "touch": return runTouch(...args);
+    case "ls": return runLs();
   }
 }
 
